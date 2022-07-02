10e9b3b01fce0133e2a2c9bd3ff47ed9d1ccb47b
Darryl Johnson <dandycodes@gmail.com>
2022-07-02 18:05:31 -0400

    files added/format and functions commented out 7/2

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+// Must create function that can keep track of high scores.
+//Script file must have ability to compare scores with peers *LOCALSTORAGE*
+//"ON-CLICK" start button that starts the timer
+//"ON-TIMER-START" presents question to the user
+// "After-Answer" presents user with another question
+//"Answer-Incorrect" presents user with subtracted time from clock/timer
+//"Answer-Correct" presents user with additional time on the clock/timer
+// "All-Answered" IF/Else "Timer-0" game is over
+//"Game-Over" user can save initials/username and score *LOCALSTORAGE*
